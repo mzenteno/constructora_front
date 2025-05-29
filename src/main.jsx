5e76278917f6ae10d3f68ba699cf805d3e4854d9
mzenteno <mzenteno.intedes@gmail.com>
2025-05-28 23:49:36 -0400

    cambios para render

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import "./index.css";
 import { AppRouter } from "@routes/AppRouter";
 import { I18nProvider } from "@store/I18nContext";
 import { AuthProvider } from "@store/AuthContext";
+import "./assets/vendors/js/vendor.bundle.base.js";
+import "./assets/js/hoverable-collapse.js";
+import "./assets/js/off-canvas.js";
+import "./assets/js/misc.js";
 
 createRoot(document.getElementById("root")).render(
   <I18nProvider>
